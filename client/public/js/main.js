e2b681b60f45cdae7a445c8ce0bb99b3097c8a61
LeDinhPhuc <ledinhphucts98@gmail.com>
2020-09-30 15:42:27 +0700

    online and join were done!

---
+++ client/public/js/main.js
@@ -0,0 +1,198 @@
+"use strict";
+const SIGNAL_SERVER = "http://localhost:5000";
+const ICE_SERVERS = [{ urls: "stun:stun.l.google.com:19302" }];
+const url = window.location.href;
+const options = { transports: ["websocket"] }; // lb
+// If you are sure the WebSocket connection will succeed, you can disable the polling transport:
+const socket = io.connect(SIGNAL_SERVER, options);
+let _peers = [];
+let peer = null;
+
+if (document.readyState) {
+  socket.on("connect", () => {
+    const socketId = socket.id;
+    peer = { name: "Hello", socketId, url, peerId: socketId };
+    socket.emit("online", {
+      newPeer: peer,
+    });
+  });
+  socket.on("online", (data) => {
+    const { peers } = data;
+    _peers = peers;
+  });
+  socket.on("newPeer", (newPeer) => {
+    _peers.push(newPeer);
+  });
+  socket.on("leave", (data) => {
+    console.log("leave ", data);
+    const { peerId } = data;
+    _peers = _peers.filter((peer) => peer.peerId !== peerId);
+    console.log({ _peers });
+  });
+}
+
+let localConnection;
+let remoteConnection;
+let sendChannel;
+let receiveChannel;
+
+const dataChannelSend = document.querySelector("textarea#dataChannelSend");
+const dataChannelReceive = document.querySelector(
+  "textarea#dataChannelReceive"
+);
+const startButton = document.querySelector("button#startButton");
+const sendButton = document.querySelector("button#sendButton");
+const closeButton = document.querySelector("button#closeButton");
+
+startButton.onclick = createConnection;
+sendButton.onclick = sendData;
+closeButton.onclick = closeDataChannels;
+
+function enableStartButton() {
+  startButton.disabled = false;
+}
+
+function disableSendButton() {
+  sendButton.disabled = true;
+}
+
+function createConnection() {
+  dataChannelSend.placeholder = "";
+  window.localConnection = localConnection = new RTCPeerConnection(ICE_SERVERS);
+  console.log("Created local peer connection object localConnection");
+
+  sendChannel = localConnection.createDataChannel("sendDataChannel");
+  console.log("Created send data channel");
+
+  localConnection.onicecandidate = (e) => {
+    onIceCandidate(localConnection, e);
+  };
+  sendChannel.onopen = onSendChannelStateChange;
+  sendChannel.onclose = onSendChannelStateChange;
+
+  window.remoteConnection = remoteConnection = new RTCPeerConnection(
+    ICE_SERVERS
+  );
+  console.log("Created remote peer connection object remoteConnection");
+
+  remoteConnection.onicecandidate = (e) => {
+    onIceCandidate(remoteConnection, e);
+  };
+  remoteConnection.ondatachannel = receiveChannelCallback;
+
+  localConnection
+    .createOffer()
+    .then(gotDescription1, onCreateSessionDescriptionError);
+  startButton.disabled = true;
+  closeButton.disabled = false;
+}
+
+function offer() {}
+
+function onCreateSessionDescriptionError(error) {
+  console.log("Failed to create session description: " + error.toString());
+}
+
+function sendData() {
+  const data = dataChannelSend.value;
+  sendChannel.send(data);
+  console.log("Sent Data: " + data);
+}
+
+function closeDataChannels() {
+  console.log("Closing data channels");
+  sendChannel.close();
+  console.log("Closed data channel with label: " + sendChannel.label);
+  receiveChannel.close();
+  console.log("Closed data channel with label: " + receiveChannel.label);
+  localConnection.close();
+  remoteConnection.close();
+  localConnection = null;
+  remoteConnection = null;
+  console.log("Closed peer connections");
+  startButton.disabled = false;
+  sendButton.disabled = true;
+  closeButton.disabled = true;
+  dataChannelSend.value = "";
+  dataChannelReceive.value = "";
+  dataChannelSend.disabled = true;
+  disableSendButton();
+  enableStartButton();
+}
+
+function gotDescription1(desc) {
+  localConnection.setLocalDescription(desc);
+  console.log(`Offer from localConnection\n${desc.sdp}`);
+  remoteConnection.setRemoteDescription(desc);
+  remoteConnection
+    .createAnswer()
+    .then(gotDescription2, onCreateSessionDescriptionError);
+}
+
+function gotDescription2(desc) {
+  remoteConnection.setLocalDescription(desc);
+  console.log(`Answer from remoteConnection\n${desc.sdp}`);
+  localConnection.setRemoteDescription(desc);
+}
+
+function getOtherPc(pc) {
+  return pc === localConnection ? remoteConnection : localConnection;
+}
+
+function getName(pc) {
+  return pc === localConnection
+    ? "localPeerConnection"
+    : "remotePeerConnection";
+}
+
+function onIceCandidate(pc, event) {
+  getOtherPc(pc)
+    .addIceCandidate(event.candidate)
+    .then(onAddIceCandidateSuccess, onAddIceCandidateError);
+  console.log(
+    `${getName(pc)} ICE candidate: ${
+      event.candidate ? event.candidate.candidate : "(null)"
+    }`
+  );
+}
+
+function onAddIceCandidateSuccess() {
+  console.log("AddIceCandidate success.");
+}
+
+function onAddIceCandidateError(error) {
+  console.log(`Failed to add Ice Candidate: ${error.toString()}`);
+}
+
+function receiveChannelCallback(event) {
+  console.log("Receive Channel Callback");
+  receiveChannel = event.channel;
+  receiveChannel.onmessage = onReceiveMessageCallback;
+  receiveChannel.onopen = onReceiveChannelStateChange;
+  receiveChannel.onclose = onReceiveChannelStateChange;
+}
+
+function onReceiveMessageCallback(event) {
+  console.log("Received Message");
+  dataChannelReceive.value = event.data;
+}
+
+function onSendChannelStateChange() {
+  const readyState = sendChannel.readyState;
+  console.log("Send channel state is: " + readyState);
+  if (readyState === "open") {
+    dataChannelSend.disabled = false;
+    dataChannelSend.focus();
+    sendButton.disabled = false;
+    closeButton.disabled = false;
+  } else {
+    dataChannelSend.disabled = true;
+    sendButton.disabled = true;
+    closeButton.disabled = true;
+  }
+}
+
+function onReceiveChannelStateChange() {
+  const readyState = receiveChannel.readyState;
+  console.log(`Receive channel state is: ${readyState}`);
+}
--- server/index.js
+++ server/index.js
@@ -1,119 +1,32 @@
-const express = require("express");
-const app = express();
-const socket = require("socket.io");
-const http = require("http");
-const server = http.createServer(app);
+const server = require("http").createServer();
+const options = { transports: ["websocket"] };
+const io = require("socket.io")(server, options);
+server.listen(5000);
 
-app.use(express.static("public"));
+let peers = [];
 
-server.listen(5000, function () {
-  console.log("Server is listening at %s port", 5000);
-});
-
-const io = socket(server);
-
-let channels = {};
-let sockets = {};
-
-io.on("connection", function (socket) {
-  let channel;
-
-  socket.channels = {};
-  sockets[socket.id] = socket;
-  console.log("[" + socket.id + "] connection accepted");
-
-  socket.on("disconnect", function (reason) {
-    console.log("reason: ", reason);
-    for (const channel in socket.channels) {
-      part(channel);
-    }
-    console.log("[" + socket.id + "] disconnected");
-    delete sockets[socket.id];
+io.on("connection", (socket) => {
+  socket.on("online", (data) => {
+    const { newPeer } = data;
+    socket.to(socket.id).emit("online", { peers }); // emit event to new connection
+    socket.broadcast.emit("newPeer", newPeer); // emit event to other connection
+    peers.push(newPeer);
   });
 
-  socket.on("join-room", function (config) {
-    if (config) {
-      channel = config.channel;
-      const { userID, userdata } = config;
-      const { name } = userdata;
-
-      if (channel in socket.channels) {
-        console.log("[" + socket.id + "] ERROR: already joined ", channel);
-        return;
-      }
-      if (!(channel in channels)) {
-        channels[channel] = {};
-      }
-      for (id in channels[channel]) {
-        channels[channel][id].emit("addPeer-room", {
-          peer_id: socket.id,
-          should_create_offer: false,
-        });
-        socket.emit("addPeer-room", { peer_id: id, should_create_offer: true });
-        console.log("What is this id => ", id);
-      }
-      console.log(name, " joining room", config.channel);
-      socket.join(config.channel);
-      socket.broadcast.in(config.channel).emit("room-users", config);
-      channels[channel][socket.id] = socket;
-      socket.channels[channel] = channel;
-    }
-  });
-
-  function part(channel) {
-    console.log("[" + socket.id + "] part ");
-    if (!(channel in socket.channels)) {
-      console.log("[" + socket.id + "] ERROR: not in ", channel);
-      return;
-    }
-    delete socket.channels[channel];
-    delete channels[channel][socket.id];
-    for (id in channels[channel]) {
-      channels[channel][id].emit("removePeer", { peer_id: socket.id });
-      socket.emit("removePeer", { peer_id: id });
-    }
-  }
-
-  socket.on("part", part);
-
-  socket.on("relayICECandidate-room", function (config) {
-    const peer_id = config.peer_id;
-    const ice_candidate = config.ice_candidate;
-    console.log(
-      "[" + socket.id + "] relaying ICE candidate to [" + peer_id + "] ",
-      ice_candidate
-    );
-    if (peer_id in sockets) {
-      sockets[peer_id].emit("iceCandidate-room", {
-        peer_id: socket.id,
-        ice_candidate: ice_candidate,
-      });
-    }
-  });
-
-  socket.on("relaySessionDescription-room", function (config) {
-    var peer_id = config.peer_id;
-    var session_description = config.session_description;
-    console.log(
-      "[" + socket.id + "] relaying session description to [" + peer_id + "] ",
-      session_description
-    );
-    if (peer_id in sockets) {
-      sockets[peer_id].emit("sessionDescription-room", {
-        peer_id: socket.id,
-        session_description: session_description,
-      });
-    }
-  });
-
-  // this for  file  transfer
-  socket.on("file-send-room", function (file) {
-    // console.log(file);
-    socket.to(channel).emit("file-out-room", file);
-  });
-
-  socket.on("file-send-room-result", function (file) {
-    // console.log(file);
-    socket.to(channel).emit("file-out-room-result", file);
+  socket.on("disconnect", (reason) => {
+    const index = peers.findIndex((peer) => peer.socketId === socket.id);
+    const { peerId } = peers[index];
+    socket.broadcast.emit("leave", { peerId });
+    peers.splice(index, 1);
   });
 });
+
+// online
+// login
+// join
+// offer
+// answer
+// cadidate
+// datachannel
+// leave
+// disconnect
